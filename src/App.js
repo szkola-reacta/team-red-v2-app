54f2eca65adca07f3fe39b4e1e019ce4d58eab2d
OlfiDell <olaf.tarlowski@gmail.com>
2022-06-14 14:53:44 +0200

    basic CSS reset added, font-size set on HTML tag, possible control with REM units, App.test.js removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App">create react app</div>;
+  return (
+    <div className="App">
+      create react app
+      <h1>Test</h1>
+    </div>
+  );
 };
 
 export default App;
