13581cc056a6b630051f628b067873a364de5e35
Pasniczy <bartoszpasnik@gmail.com>
2022-06-16 18:55:39 +0200

    Extend eslint config and rules

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: ["plugin:prettier/recommended", "airbnb", "airbnb/hooks", "react-app", "plugin:react/jsx-runtime"],
+  plugins: ["prettier", "unused-imports"],
+  rules: {
+    "prettier/prettier": 2,
+    semi: 2,
+    quotes: [2, "double"],
+    "no-undef": 0,
+    "no-shadow": 0,
+    "arrow-body-style": 0,
+    "no-nested-ternary": 2,
+    "no-trailing-spaces": 2,
+    "unused-imports/no-unused-imports": 1,
+    "jsx-quotes": ["error", "prefer-double"],
+    "react/function-component-definition": 0,
+    "react/jsx-filename-extension": [0, { extensions: [".js", ".jsx"] }],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  jsxQuotes: "prefer-single",
+  jsxBracketSameLine: true,
+};
