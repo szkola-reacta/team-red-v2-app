ab9a4d48a9ce318f7e102994d3f264a407705535
Piotr Szymaniec <traf@angrenost.org>
2022-08-19 19:07:53 +0200

    feat: add retrieving info about GH Team members

--- src/services/github.js
+++ src/services/github.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-export default function getDataFromGithubEndpoint(url) {
+export function getDataFromGithubEndpoint(url) {
   // GitHub token with read:org scope that never expires
   const INFINITE_GITHUB_ORG_TOKENT = "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";
   const GITHUB_API_URL = "https://api.github.com";
@@ -13,4 +13,8 @@ export default function getDataFromGithubEndpoint(url) {
   };
 
   return axios(config);
+}
+
+export default function getTeamMembers() {
+  return getDataFromGithubEndpoint("/organizations/53264097/team/6194099/members");
 }
