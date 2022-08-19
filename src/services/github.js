2111d0051534478e12985f5c699d894ab9d9636e
Piotr Szymaniec <traf@angrenost.org>
2022-08-19 22:18:35 +0200

    refactor: add baseURL to axios config

--- src/services/github.js
+++ src/services/github.js
@@ -1,12 +1,14 @@
 const axios = require("axios");
 
+const INFINITE_GITHUB_ORG_TOKENT = "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";
+const GITHUB_API_URL = "https://api.github.com";
+
 export function getDataFromGithubEndpoint(url) {
   // GitHub token with read:org scope that never expires
-  const INFINITE_GITHUB_ORG_TOKENT = "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";
-  const GITHUB_API_URL = "https://api.github.com";
   const config = {
     method: "get",
-    url: `${GITHUB_API_URL}${url}`,
+    baseURL: GITHUB_API_URL,
+    url,
     headers: {
       Authorization: `Token ${INFINITE_GITHUB_ORG_TOKENT}`,
     },
