0d6a372ebaf497adb0595c4a3a322263d773d13b
SAK74 <sergijks@gmail.com>
2022-08-26 20:44:02 +0200

    adjustment to eslint

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import { data } from "../components/team/mockData";
 // import getTeamMembers from "services/github";
 import { Card } from "components/team/Card";
+import { data } from "../components/team/mockData";
 import "components/team/style.scss";
 
 function Team() {
