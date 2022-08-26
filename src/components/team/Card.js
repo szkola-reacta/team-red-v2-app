ef15def1131ad75a1ae94eb29c6763c7fd5c59b9
SAK74 <sergijks@gmail.com>
2022-08-26 20:22:05 +0200

    restructrured from MUI to sass styling

--- src/components/team/Card.js
+++ src/components/team/Card.js
@@ -1,37 +1,9 @@
-import {
-  Card as MUICard,
-  styled,
-  CardMedia,
-  CardContent,
-  CardActions,
-  IconButton,
-  Box,
-  Collapse,
-  CardActionArea,
-  Link,
-} from "@mui/material";
-import { ExpandMore as ExpandIcon, Star as StarIcon } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { getDataFromGithubEndpoint } from "services/github";
 
-const CustomCard = styled(MUICard)({
-  width: 150,
-  borderRadius: 20,
-});
-
-const ExpandMore = styled(({ expand, ...other }) => {
-  return <IconButton {...other} children={<ExpandIcon />} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
 export const Card = ({ user }) => {
   const { avatar_url, login, html_url, starred_url } = user;
-  const [expand, setExpand] = useState(false);
+  const [expand, setExpand] = useState(false); // controll of expanded area
   const [starred, setStarred] = useState(null);
   useEffect(() => {
     // get amount starred repo
@@ -40,35 +12,17 @@ export const Card = ({ user }) => {
     );
   }, [starred_url]);
 
+  // todo styling !
   return (
-    <CustomCard>
-      <CardActionArea>
-        <Link href={html_url} underline="none" color="inherit" target="_blank">
-          <CardMedia
-            component="img"
-            image={avatar_url}
-            sx={{
-              borderRadius: "50%",
-            }}
-          />
-          <CardContent children={login} />
-        </Link>
-      </CardActionArea>
-      <CardActions children={<ExpandMore expand={expand} onClick={() => setExpand(!expand)} />} />
-      <Collapse
-        in={expand}
-        unmountOnExit
-        children={
-          <CardContent
-            children={
-              <Box>
-                <StarIcon color="primary" />
-                {starred}
-              </Box>
-            }
-          />
-        }
-      />
-    </CustomCard>
+    <div className="team__card">
+      <a href={html_url} target="_blank" rel="noreferrer">
+        <img src={avatar_url} alt={login} />
+        <div>{login}</div>
+      </a>
+      <button type="button" onClick={() => setExpand(!expand)}>
+        {!expand ? "Show more" : "Show less"}
+      </button>
+      <div style={{ display: expand ? "block" : "none" }}>{`Starred repo:  ${starred}`}</div>
+    </div>
   );
 };
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
-import { Box } from "@mui/material";
-// import { data } from "../components/team/mockData";
-import getTeamMembers from "services/github";
+import { data } from "../components/team/mockData";
+// import getTeamMembers from "services/github";
 import { Card } from "components/team/Card";
+import "components/team/style.scss";
 
 function Team() {
-  const [members, setMembers] = useState(null);
+  const [members, setMembers] = useState([]);
   useEffect(() => {
-    getTeamMembers().then((data) => setMembers(data));
-    // setMembers(data);
+    // getTeamMembers().then((data) => setMembers(data));
+    setMembers(data);
   }, []);
   // console.log(members);
 
@@ -16,20 +16,11 @@ function Team() {
     <>
       <h1>Our Team</h1>
       <p>Quality code from a quality team!</p>
-      {members && (
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: 5,
-            mt: 5,
-          }}
-        >
-          {members.map((user) => (
-            <Card key={user.id} user={user} />
-          ))}
-        </Box>
-      )}
+      <div className="team__container">
+        {members.map((user) => (
+          <Card key={user.id} user={user} />
+        ))}
+      </div>
     </>
   );
 }
