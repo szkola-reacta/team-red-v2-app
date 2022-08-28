59a23ecbe5d768d55d0f7cacd64d9daf577d664f
Poocinka15 <poociek15@gmail.com>
2022-08-28 22:58:34 +0200

    eslint adjustment

--- src/components/team/Card.js
+++ src/components/team/Card.js
@@ -22,10 +22,9 @@ export const Card = ({ user }) => {
       <button className="team__button" type="button" onClick={() => setExpand(!expand)}>
         {!expand ? "Show more" : "Show less"}
       </button>
-      <div
-        className="team__more-info"
-        style={{ display: expand ? "block" : "none" }}
-      >{`Starred repo:  ${starred}`}</div>
+      <div className="team__more-info" style={{ display: expand ? "block" : "none" }}>
+        {`Starred repo:  ${starred}`}
+      </div>
     </div>
   );
 };
