7fb42928760897a2b28ba43ca4e07bb09e11c530
Misiorny <krzystan@wp.pl>
2022-08-31 19:58:53 +0200

    feat: delete animation

--- src/components/layout/Logo.js
+++ src/components/layout/Logo.js
@@ -1,11 +1,9 @@
 import LogoOne from "../../img/logo/LogoOne.png";
-import LogoTwo from "../../img/logo/LogoTwo.png";
 
 function Logo() {
   return (
     <div className="header__logo">
       <img className="header__logo-one" src={LogoOne} alt="logo" />
-      <img className="header__logo-two" src={LogoTwo} alt="logo" />
     </div>
   );
 }
